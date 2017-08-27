f3b3d176a7364c283a3d028ccaf7dfc03ba2190d
likaixuan <1375502718@qq.com>
2017-08-27 20:38:41 +0800

    添加职责链

--- src/util.js
+++ src/util.js
@@ -4,12 +4,35 @@ let util = {};
 //类型检测 is···
 (function (util, arr) {
     let type = "";
-    for ( let i = 0, type; type = arr[i++]; ) {
+    for (let i = 0, type; type = arr[i++];) {
         util['is' + type] = function (obj) {
-            return Object.prototype.toString.call(obj) === '[object '+type+']';
+            return Object.prototype.toString.call(obj) === '[object ' + type + ']';
         }
     }
 
-})(util, ['String', 'Array', 'Number','Object','Function','Null','Undefined']);
+})(util, ['String', 'Array', 'Number', 'Object', 'Function', 'Null', 'Undefined']);
+
+//职责链
+util.Chain = function (fn) {
+    this.fn = fn;
+    this.successor = null;
+}
+
+util.Chain.prototype.setNextSuccessor = function (successor) {
+    //设置下一个节点
+    return this.successor = successor;
+}
+
+util.Chain.prototype.passRequest = function () {
+
+    //执行当前节点
+    let ret = this.fn.apply(this, arguments, this.next);
+    return ret;
+};
+
+util.Chain.prototype.next = function () {
+    //执行下一个节点
+    return this.successor && this.successor.passRequest.apply(this.successor, arguments);
+}
 
 export default util;
